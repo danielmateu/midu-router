447fc377b19c42b425f308a7f10032deb0e968aa
Daniel Mateu <danielmateu86@gmail.com>
2023-03-13 11:38:48 +0100

    LazyLoading de las paginas Done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
 
-import NotFoundPage from './pages/404'
-import AboutPage from './pages/About'
-import HomePage from './pages/Home'
-import SearchPage from './pages/Search'
+import { Suspense, lazy } from 'react'
+// import NotFoundPage from './pages/404'
+// import AboutPage from './pages/About' //Import estático
+// import HomePage from './pages/Home'
+// import SearchPage from './pages/Search'
 
 import { Route } from './Route'
 import { Router } from './Router'
 
+const LazyAboutPage = lazy(() => import('./pages/About.jsx'))// Import dinámico
+const LazyHomePage = lazy(() => import('./pages/Home.jsx'))
+const LazySearchPage = lazy(() => import('./pages/Search.jsx'))
+const LazyNotFoundPage = lazy(() => import('./pages/404.jsx'))
+
 const appRoutes = [
-  // {
-  //   path: '/',
-  //   component: HomePage
-  // }
-  // {
-  //   path: '/about',
-  //   component: AboutPage
-  // },
   {
     path: '/search/:query',
-    component: SearchPage
+    component: LazySearchPage
   }
 ]
 
 function App () {
   return (
     <main>
-      {/* <Router routes={routes} defaultComponent /> */}
-      <Router routes={appRoutes} defaultComponent={NotFoundPage}>
-        <Route path='/' component={HomePage} />
-        <Route path='/about' component={AboutPage} />
-      </Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        {/* <Router routes={routes} defaultComponent /> */}
+        <Router routes={appRoutes} defaultComponent={LazyNotFoundPage}>
+          <Route path='/' component={LazyHomePage} />
+          <Route path='/about' component={LazyAboutPage} />
+        </Router>
+      </Suspense>
     </main>
   )
 }
